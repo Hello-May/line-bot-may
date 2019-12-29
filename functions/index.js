dabe6dabd8d96e150a0514f37c54edd1e18307f8
May <jailbait7758@gmail.com>
2019-12-29 13:52:59 +0800

    update

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,7 @@ const config = require('../config');// 導入設定檔
 const client = new line.Client(config);
 
 const main = require('./main');
-const main = require('./test');
+// const main = require('./test');
 
 // var shutUp;
 
