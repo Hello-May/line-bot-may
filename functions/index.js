8088a5597edd11760bc7129b54e5fbc907366b35
May <jailbait7758@gmail.com>
2020-01-01 15:51:10 +0800

    update

--- functions/index.js
+++ functions/index.js
@@ -37,6 +37,7 @@ const textCommandSolver = (event) => {
                     let s = new Date(event.timestamp);
                     let d = s.getFullYear() + "-" + (s.getMonth() < 10 ? '0' + (s.getMonth() + 1) : (s.getMonth() + 1)) + "-" + (s.getDate() < 10 ? '0' + s.getDate() : s.getDate());
                     let t = (s.getHours() < 10 ? '0' + s.getHours() : s.getHours()) + ":" + (s.getMinutes() < 10 ? '0' + s.getMinutes() : s.getMinutes()) + ":" + (s.getSeconds() < 10 ? '0' + s.getSeconds() : s.getSeconds());
+                    console.log(d + ' ' + t);
                     msg = d + ' ' + t + '\n你剛剛說了：' + input;
                 }
         }
