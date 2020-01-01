74316f3ed3daf2030251aefbcb7d351f05ef2983
May <jailbait7758@gmail.com>
2020-01-01 14:47:42 +0800

    update

--- functions/index.js
+++ functions/index.js
@@ -34,7 +34,7 @@ const textCommandSolver = (event) => {
                 try {
                     msg = '答案是' + math.eval(input.toLowerCase()).toString();
                 } catch (err) {
-                    msg = Date.now() + '~你剛剛說了：' + input;
+                    msg = line.Client.name + '~你剛剛說了：' + input;
                 }
         }
     }
