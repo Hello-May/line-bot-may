84aea5a246951d4a300e90fe84c745c391477749
May <jailbait7758@gmail.com>
2020-01-03 20:33:49 +0800

    update

--- functions/index.js
+++ functions/index.js
@@ -32,8 +32,55 @@ const textCommandSolver = (event) => {
                 msg = main.test(event);
                 break;
             case '.':
-                msg = ['Hello, world 1', 'Hello, world 2'];
-                break;
+                return client.replyMessage(event.replyToken, 
+                    {
+                        "type": "flex",
+                        "altText": "Flex Message",
+                        "contents": {
+                          "type": "bubble",
+                          "direction": "ltr",
+                          "header": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                              {
+                                "type": "text",
+                                "text": "鬥獸",
+                                "size": "lg",
+                                "align": "center",
+                                "weight": "bold",
+                                "color": "#000000"
+                              }
+                            ]
+                          },
+                          "hero": {
+                            "type": "image",
+                            "url": "https://images.clipartlogo.com/files/istock/previews/9083/90832359-community-team-friendship-togetherness-icon-teamwork-social-group-concept-line.jpg",
+                            "size": "xl",
+                            "aspectRatio": "1.51:1",
+                            "aspectMode": "fit"
+                          },
+                          "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                              {
+                                "type": "separator"
+                              },
+                              {
+                                "type": "text",
+                                "text": "blabla",
+                                "margin": "xxl",
+                                "align": "start",
+                                "gravity": "center",
+                                "weight": "bold",
+                                "wrap": true
+                              }
+                            ]
+                          }
+                        }
+                      }
+                );
             default:
                 try {
                     msg = '答案是' + math.eval(input.toLowerCase()).toString();
