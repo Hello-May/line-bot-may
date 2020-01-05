2b6bd798b72977c23255df0e13e6386c8c5f2ff3
May <jailbait7758@gmail.com>
2020-01-05 17:15:09 +0800

    update

--- config/notify.js
+++ config/notify.js
@@ -2,11 +2,11 @@ const configNotify = {
     clientId: '3qZCtZ2rmMdUcDa3qhFfyM',
     clientSecret: 'GTFK3GaIa77WzB08gS9RH4446gpoZFRKd3YEineEJES',
     channelAccessToken:'ZvIR3CGvofbTvYcSHozeHrzuPL3TUowrNzr3xIH10w5',
-    AuthorizeApi = 'https:\/\/notify-bot.line.me\/oauth\/authorize',
-    TokenApi = 'https:\/\/notify-bot.line.me\/oauth\/token',
-    NotifyApi = 'https:\/\/notify-api.line.me\/api\/notify',
-    StatusApi = 'https:\/\/notify-api.line.me\/api\/status',
-    RevokeApi = 'https:\/\/notify-api.line.me\/api\/revoke'
+    AuthorizeApi: 'https:\/\/notify-bot.line.me\/oauth\/authorize',
+    TokenApi: 'https:\/\/notify-bot.line.me\/oauth\/token',
+    NotifyApi: 'https:\/\/notify-api.line.me\/api\/notify',
+    StatusApi: 'https:\/\/notify-api.line.me\/api\/status',
+    RevokeApi: 'https:\/\/notify-api.line.me\/api\/revoke'
 }
 
 // app.use('/linenotify',
