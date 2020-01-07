b8d1bb37826296a447b2fe7298284a58f826a5c1
May <jailbait7758@gmail.com>
2020-01-07 11:46:08 +0800

    update

--- functions/index.js
+++ functions/index.js
@@ -32,13 +32,14 @@ const textCommandSolver = (event) => {
                 db.sql.query('select * from student', function (err, result, fiels) {
                     if (err) throw err;
                     var r = result[0].name;
+                    console.log(r);
+                    output = {
+                        type: 'text',
+                        text: r
+                    }
+    
                 });
-                console.log(r);
-                output = {
-                    type: 'text',
-                    text: r
-                }
-
+               
                 break;
             case '.':
                 output = lineNotify.test();
