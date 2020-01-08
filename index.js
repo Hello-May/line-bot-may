f71561b24322dba4f7fa086f9311b539abf3b824
May <jailbait7758@gmail.com>
2020-01-08 13:07:47 +0800

    update

--- index.js
+++ index.js
@@ -57,79 +57,31 @@ app.get("/button", function (req, res) {
 //       });
 // })
 
-// app.listen(process.env.PORT || 8080, function () {
-//   console.log('App now running on port', this.address().port);
-// });
-
-
-// const extendTimeoutMiddleware = (req, res, next) => {
-//   const space = ' ';
-//   let isFinished = false;
-//   let isDataSent = false;
-
-//   // Only extend the timeout for API requests
-//   if (!req.url.includes('/api')) {
-//     next();
-//     return;
-//   }
-
-//   res.once('finish', () => {
-//     isFinished = true;
-//   });
-
-//   res.once('end', () => {
-//     isFinished = true;
-//   });
-
-//   res.once('close', () => {
-//     isFinished = true;
-//   });
-
-//   res.on('data', (data) => {
-//     // Look for something other than our blank space to indicate that real
-//     // data is now being sent back to the client.
-//     if (data !== space) {
-//       isDataSent = true;
-//     }
-//   });
-
-//   const waitAndSend = () => {
-//     setTimeout(() => {
-//       // If the response hasn't finished and hasn't sent any data back....
-//       if (!isFinished && !isDataSent) {
-//         // Need to write the status code/headers if they haven't been sent yet.
-//         if (!res.headersSent) {
-//           res.writeHead(202);
-//         }
-
-//         res.write(space);
-
-//         // Wait another 15 seconds
-//         waitAndSend();
-//       }
-//     }, 15000);
-//   };
-
-//   waitAndSend();
-//   next();
-// };
-
-// app.use(extendTimeoutMiddleware);
-
 const DelayedResponse = require('http-delayed-response');
 
-app.post('/', lineBot.middleware(configBot), function (req, res,next) {
-  function slowfunction() {
-  return Promise
+function slowFunction (callback) {
+  // let's do something that could take a while...
+  console.log('wait');
+}
+ 
+app.use(function (req, res) {
+  var delayed = new DelayedResponse(req, res);
+  slowFunction(delayed.wait());
+});
+
+app.post('/', lineBot.middleware(configBot), function (req, res) {
+Promise
     .all(req.body.events.map(handleEvent))
     .then(function (result) {
       res.json(result);
     });
-  }
-    const delayed = new DelayedResponse(req, res, next);
-    slowfunction(delayed.start(10 * 1000, 10 * 1000));
 });
 
-app.listen(process.env.PORT || port, function () {
+app.listen(process.env.PORT || 8080, function () {
   console.log('App now running on port', this.address().port);
 });
+
+
+// app.listen(process.env.PORT || port, function () {
+//   console.log('App now running on port', this.address().port);
+// });
