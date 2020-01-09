8ffc2f27b9763f053a875ec3a2f0e9ae68e8f817
May <jailbait7758@gmail.com>
2020-01-09 14:08:47 +0800

    update

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ app.get("/regisToken", (req, res, next) => {
   res.sendFile(path.resolve('./functions/notify/res.html'), function (err) {
     if (err) res.send(404);
   });
+  console.log(req);
   console.log(token+'<-----------------------------------');
 });
 
