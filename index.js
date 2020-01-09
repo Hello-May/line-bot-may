45b094aff5928d0137459a6ba765f4e16cc3401f
May <jailbait7758@gmail.com>
2020-01-09 13:59:00 +0800

    update

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.get("/button", function (req, res) {
 });
 
 app.get("/regisToken", (req, res, next) => {
-  let token = res.params.code;
+  let token = req.params.code;
   console.log(token+'<-----------------------------------');
 })
 
