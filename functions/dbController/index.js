d9c67719181483f416953a4b1fd25a02010c5102
May <jailbait7758@gmail.com>
2020-01-11 05:00:00 +0800

    update

--- functions/dbController/index.js
+++ functions/dbController/index.js
@@ -93,12 +93,12 @@ const getToken = async (event) => {
     // return new Promise( async (resolve, reject) => {
         switch (event.source.type) {
             case 'user':
-                let user = await users.findAll({ where: { userId: event.source.userId } });
+                let user = await users.findOne({ where: { userId: event.source.userId } });
                 console.log(user);
                 console.log(user.token);
                 return user.token;
             case 'group':
-                let group = await groups.findAll({ where: { groupId: event.source.groupId } });
+                let group = await groups.findOne({ where: { groupId: event.source.groupId } });
                 console.log(group);
                 console.log(group.token);
                 return group.token;
