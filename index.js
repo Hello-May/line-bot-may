11a116c8b44300dee5284c0f18deef6ebeb7a005
May <jailbait7758@gmail.com>
2020-01-12 11:24:56 +0800

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function handleEvent(event) {
     case 'message':
       switch (event.message.type) {
         case 'text':
-          return functions.textCommandSolver(event);
+          return functions.textCommandSolver(event).catch(error => { console.log(err);});
         case 'image':
           return functions.imgCommandSolver(event);
         case 'sticker':
