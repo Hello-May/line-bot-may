38c240c2e41b181aaeb92de9633ef1b96f027a26
May <jailbait7758@gmail.com>
2020-01-12 14:45:52 +0800

    update

--- functions/dbController/user.js
+++ functions/dbController/user.js
@@ -5,7 +5,7 @@ const User = db.users;
 const searchById = async (id) => {
     let user = await users.findAll({ where: { userId: id } });
     console.log("<--------------------user start");
-    console.log(user[0]);
+    // console.log(user[0]);
     console.log("<--------------------user end");
     return user[0];
 }
