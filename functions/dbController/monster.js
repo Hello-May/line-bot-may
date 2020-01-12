b9ea573909797dc729226d929c3006395461d92f
May <jailbait7758@gmail.com>
2020-01-12 08:15:18 +0800

    update

--- functions/dbController/monster.js
+++ functions/dbController/monster.js
@@ -3,8 +3,8 @@ const { monsters } = require("../../models");
 const Monster = db.monsters;
 
 const searchById = (id) => {
+    try {
     return new Promise(async (resolve, reject) => {
-        try {
             let monster = await monsters.findAll({ where: { monsterId: id } });
             console.log(monster[0]);
             resolve(monster[0]);
--- functions/dbController/user.js
+++ functions/dbController/user.js
@@ -3,8 +3,8 @@ const { users } = require("../../models");
 const User = db.users;
 
 const searchById = (id) => {
-    return new Promise(async (resolve, reject) => {
-        try {
+    try {
+    return new Promise(async (resolve, reject) => { 
             let user = await users.findAll({ where: { userId: id } });
             console.log(user[0]);
             resolve(user[0]);
