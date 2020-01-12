336cc48b1fc43060fd7f210b64ae779f88b90018
May <jailbait7758@gmail.com>
2020-01-12 08:53:33 +0800

    update

--- functions/main/monster.js
+++ functions/main/monster.js
@@ -14,9 +14,7 @@ const monster = async (event) => {
         console.log(user.monsterId + "<-----------------------------monsterId");
         let monster = await dbMonster.searchById(user.monsterId);
         console.log(monster.name + "<-------------------------monster.name")
-    } catch (err) {
-        console.log(err);
-    }
+    
     return {
         "type": "flex",
         "altText": "Flex Message",
@@ -128,6 +126,9 @@ const monster = async (event) => {
             }
         }
     }
+} catch (err) {
+    return err;
+}
 }
 
 module.exports = {
