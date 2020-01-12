38c1894a49b19d62bc4694851836d3bea30dfe52
May <jailbait7758@gmail.com>
2020-01-12 08:04:02 +0800

    update

--- functions/main/monster.js
+++ functions/main/monster.js
@@ -16,6 +16,7 @@ const monster = async (event) => {
         console.log(monster.name + "<-------------------------monster.name")
     } catch (err) {
         console.log(err);
+        throw error;
     }
     return {
         "type": "flex",
