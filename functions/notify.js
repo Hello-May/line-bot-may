367e32afa00e5056559ba83e1a05d7ae194e830c
May <jailbait7758@gmail.com>
2020-01-15 05:00:30 +0800

    update

--- functions/notify.js
+++ functions/notify.js
@@ -9,19 +9,21 @@ const schedule = require('node-schedule');
 //先查habit表中的時間 存下userId 在查user表的token 如果沒token
 
 const send = () => {
-  schedule.scheduleJob('30 * * * * *',async function () {
+  schedule.scheduleJob('15 * * * * *',async function () {
     // lineNotify.notify('XNVldsovy2m6RTMubkUrIPM5FRIFXvDUir4G0Dq75eX', {
     //   type: 'message',
     //   text: '測試每分鐘30秒:' + new Date().toLocaleString('zh-TW', { timeZone: 'Asia/Taipei' })
     // });
-    // try {
     let date = new Date();
     let hour = date.getHours();
     let min = date.getMinutes();
     let habitData = await dbHabit.searchByTime((hour < 10 ? '0' + hour : hour), (min < 10 ? '0' + min : min));
+    console.log(JSON.stringify(habitData));
 
     for (let i; i < habitData.length; i++) {
       let token = await dbUser.getToken(habitData[i].userId);
+      console.log(token+'<------------------token');
+      console.log(habitData[i].habit+'<------------------habit');
       if (token != 'null') {
         lineNotify.notify(token, {
           type: 'message',
