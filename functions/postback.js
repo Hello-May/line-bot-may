1c7dd37e4f4ceb41ccf89e5e13470af6e9ff00f2
May <jailbait7758@gmail.com>
2020-01-16 16:28:13 +0800

    update

--- functions/postback.js
+++ functions/postback.js
@@ -102,7 +102,7 @@ const postbackCommandSolver = async (event, status) => {
             case '查詢指令':
                 output = {
                     type: 'text',
-                    text: '[指令]說明\n[#]叫出選單\n[#連動]連動line notify\n[#呼叫]啟動機器人\n[#閉嘴]關閉機器人\n[#修煉]選單1\n[#任務]選單2\n[#小怪獸]選單3\n[#戰鬥]選單4\n[#酒館]選單5'
+                    text: '[指令] 說明\n[#] 叫出選單\n[#連動] 連動line notify\n[#呼叫] 啟動機器人\n[#閉嘴] 關閉機器人\n[#修煉] 選單1\n[#任務] 選單2\n[#小怪獸] 選單3\n[#戰鬥] 選單4\n[#酒館] 選單5'
                 };
                 break;
             case '抖內開發者':
