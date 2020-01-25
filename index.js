639051cee7430fe3ec532cb517cdd7a6b0c1ccc9
May <jailbait7758@gmail.com>
2020-01-25 16:53:05 +0800

    update

--- index.js
+++ index.js
@@ -130,9 +130,10 @@ app.get("/send", function (req, res) {
 //   });
 
 app.post("/saveimage", function (req, res) {
-  console.log(JSON.stringify(req));
+  console.log(req);
+  // console.log(req);
   console.log("-------------------");
-  console.log(JSON.stringify(req.form));
+  // console.log(JSON.stringify(req.form));
   let data = req.form.data;
   console.log(data+"<------data");
   let matches = data.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/), imageBuffer = {};
