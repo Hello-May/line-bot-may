3fdd0e3e8e240a37bc078e2fb80e848004c6f1d7
May <jailbait7758@gmail.com>
2020-01-25 14:21:37 +0800

    update

--- functions/dbController/skin.js
+++ functions/dbController/skin.js
@@ -3,7 +3,7 @@ const { skins } = require("../../models");
 const Skin = db.skins;
 
 const searchByNameAndRandom = async (name) => {
-  let skin = await skins.findAll();
+  let skin = await skins.findAll({ where: { name: name } });
   return skin[Math.round((Math.random() * (skin.length - 1)))];
 }
 
