37721448a677c57d6e61c63e6069310a4746dd83
May <jailbait7758@gmail.com>
2020-01-26 11:11:25 +0800

    update

--- functions/postback.js
+++ functions/postback.js
@@ -100,7 +100,11 @@ const postbackCommandSolver = async (event, status) => {
     } else {
         switch (input) {
             case '戰鬥隨機':
-                output = pk.target(event);
+                // output = pk.target(event);
+                output =  {
+                    type: 'text',
+                    text: '測試'
+                };
                 break;
             case '查詢指令':
                 output = {
