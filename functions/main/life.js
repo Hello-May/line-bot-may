9926fd5d49390796dbe4c938d8af83b4157579bf
May <jailbait7758@gmail.com>
2020-01-28 23:51:41 +0800

    update

--- functions/main/life.js
+++ functions/main/life.js
@@ -179,6 +179,7 @@ function genBySkin(skin) {
 
 const skinStore = () => {
     let output = genBySkin(skin);
+    console.log(output);
     return {
         "type": "flex",
         "altText": "Flex Message",
