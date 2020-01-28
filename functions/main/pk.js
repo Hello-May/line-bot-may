925143db8b8e54127aacfb2c537db2cb2bd1ae50
May <jailbait7758@gmail.com>
2020-01-28 13:32:48 +0800

    update

--- functions/main/pk.js
+++ functions/main/pk.js
@@ -527,7 +527,7 @@ const round = async (userId, next, focus) => {
     let target;
     let player;
     for (let i = 0; i < battle.length; i++) {
-        if (battle[i].target == true) {
+        if (battle[i].identity == true) {
             target = battle[i];
         } else {
             player = battle[i];
