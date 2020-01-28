db27a2efa22427be86e71426d251af7f13549fc3
May <jailbait7758@gmail.com>
2020-01-28 14:08:22 +0800

    update

--- functions/index.js
+++ functions/index.js
@@ -18,6 +18,7 @@ const dbUser = require('./dbController/user');
 const dbMonster = require('./dbController/monster');
 const dbTask = require('./dbController/task');
 const dbHabit = require('./dbController/habit');
+const dbBattle = require('./dbController/battle');
 const date = new Date().Format("yyyy/MM/dd");
 const sticker = [[1, 2], [1, 4], [1, 5], [1, 13], [1, 14], [1, 114], [1, 119], [1, 125], [1, 132], [1, 134], [1, 137], [1, 138], [1, 139], [1, 407], [2, 34], [2, 45], [2, 144], [2, 164], [2, 166], [2, 171], [2, 172], [2, 516], [3, 180], [3, 184], [3, 186], [3, 195], [3, 200]];
 
