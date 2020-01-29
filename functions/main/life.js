7c346d44f876e6b8af9a5fb07428969014de82f6
May <jailbait7758@gmail.com>
2020-01-30 07:48:52 +0800

    update

--- functions/main/life.js
+++ functions/main/life.js
@@ -155,7 +155,7 @@ function genByProps(props) {
             "contents": [
                 {
                     "type": "text",
-                    "text": '\小小的藥丸，大大的威力。',
+                    "text": '\n小小的藥丸，大大的威力。\n(目前還不能買)',
                     "margin": "lg",
                     "align": "center",
                     "wrap": true
