39e7ff95a120a1700a9085f228512367047fcc8a
May <jailbait7758@gmail.com>
2020-02-12 19:55:08 +0800

    update

--- functions/pause.js
+++ functions/pause.js
@@ -1,10 +1,10 @@
 const pause = (event) => {
     let msg = '無此功能';
     switch (event.message.text) {
-        case '呼叫':
+        case '#呼叫':
             msg = "我在這~"
             break;
-        case '閉嘴':
+        case '#閉嘴':
             msg = "哼~不理你了~"
             break;
     }
